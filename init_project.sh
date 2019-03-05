f85d9a3d35ebfedf45f98efeb9f09999a822e5db
Alex Paliarush <paliarus@adobe.com>
2019-03-05 16:08:34 -0600

    Testing Travis Integration

--- init_project.sh
+++ init_project.sh
@@ -229,11 +229,11 @@ cd "${vagrant_dir}"
 if [[ $(isMinikubeRunning) -eq 0 ]]; then
     status "Starting minikube"
     #echo "$(python -c 'import os,sys;print(os.path.realpath("."));')/ -alldirs -mapall="$(id -u)":"$(id -g)" $(minikube ip)" | sudo tee -a /etc/exports && sudo nfsd restart
-    sudo minikube start  --vm-driver=none --bootstrapper=kubeadm --kubernetes-version=v1.13.0
+    sudo minikube start --cache-images --cpus=2 --memory=4096  --vm-driver=none --bootstrapper=kubeadm --kubernetes-version=v1.13.0
     minikube addons enable ingress
     minikube addons enable heapster
     # hanged in some cases todo
-#    sudo minikube start  --vm-driver=none --bootstrapper=kubeadm --kubernetes-version=v1.13.0 2> >(logError) | {
+#    sudo minikube start --cache-images --cpus=2 --memory=4096  --vm-driver=none --bootstrapper=kubeadm --kubernetes-version=v1.13.0 2> >(logError) | {
 #      while IFS= read -r line
 #      do
 #        filterVagrantOutput "${line}"

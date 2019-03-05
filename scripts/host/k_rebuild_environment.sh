073fab6cd03bb1ae2aa830359417c83e04245a6c
Alex Paliarush <paliarus@adobe.com>
2019-03-05 15:47:12 -0600

    Testing Travis Integration

--- scripts/host/k_rebuild_environment.sh
+++ scripts/host/k_rebuild_environment.sh
@@ -5,8 +5,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.." && vagrant_dir=$PWD
 source "${vagrant_dir}/scripts/functions.sh"
 
 ## TODO: Add status messages
-cd "${vagrant_dir}/scripts" && eval $(minikube docker-env) && docker build -t magento2-monolith:dev -f ../etc/docker/monolith/Dockerfile ../scripts
-cd "${vagrant_dir}/scripts" && eval $(minikube docker-env) && docker build -t magento2-monolith:dev-xdebug -f ../etc/docker/monolith-with-xdebug/Dockerfile ../scripts
+cd "${vagrant_dir}/scripts" && docker build -t magento2-monolith:dev -f ../etc/docker/monolith/Dockerfile ../scripts
+cd "${vagrant_dir}/scripts" && docker build -t magento2-monolith:dev-xdebug -f ../etc/docker/monolith-with-xdebug/Dockerfile ../scripts
 
 # TODO: Repeat for other deployments, not just Magento 2
 # See https://github.com/kubernetes/kubernetes/issues/33664#issuecomment-386661882
@@ -15,7 +15,7 @@ cd "${vagrant_dir}/scripts" && eval $(minikube docker-env) && docker build -t ma
 # TODO: Delete does not work when no releases created yet
 cd "${vagrant_dir}/etc/helm"
 set +e
-helm list -q | xargs helm delete --purge
+#helm list -q | xargs helm delete --purge
 set -e
 
 # TODO: Need to make sure all resources have been successfully deleted before the attempt of recreating them

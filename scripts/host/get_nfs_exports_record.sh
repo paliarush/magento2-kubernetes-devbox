315c56e1a8d2241db8dd5002664410e2cdc680e6
Alex Paliarush <paliarus@adobe.com>
2019-04-15 16:41:54 -0500

    Revert "NFS"
    This reverts commit 39895a79

--- scripts/host/get_nfs_exports_record.sh
+++ scripts/host/get_nfs_exports_record.sh
@@ -10,7 +10,7 @@ host_os="$(bash "${vagrant_dir}/scripts/host/get_host_os.sh")"
 if [[ ${host_os} == "OSX" ]]; then
     nfs_exports_record="\"${vagrant_dir}\" -alldirs -mapall=$(id -u):$(id -g) -mask 255.0.0.0 -network 192.0.0.0"
 elif [[ ${host_os} == "Linux" ]]; then
-    nfs_exports_record="\"${vagrant_dir}\" 172.17.0.0/255.255.0.0(rw,no_subtree_check,all_squash,anonuid=$(id -u),anongid=$(id -g))"
+    nfs_exports_record="\"${vagrant_dir}\" 172.17.0.0/255.255.0.0(rw,no_subtree_check,all_squash,anonuid=2000,anongid=2000)"
 else
     error "Host OS is not supported"
     exit 1

8a014c346700e3a75aeaf815194c42fc90e7fe94
Alex Paliarush <paliarus@adobe.com>
2019-04-16 14:17:05 -0500

    Revert "Revert "Revert "Revert "NFS 1""""
    This reverts commit b45179ab

--- scripts/host/get_nfs_exports_record.sh
+++ scripts/host/get_nfs_exports_record.sh
@@ -10,11 +10,10 @@ host_os="$(bash "${vagrant_dir}/scripts/host/get_host_os.sh")"
 if [[ ${host_os} == "OSX" ]]; then
     nfs_exports_record="\"${vagrant_dir}\" -alldirs -mapall=$(id -u):$(id -g) -mask 255.0.0.0 -network 192.0.0.0"
 elif [[ ${host_os} == "Linux" ]]; then
-#    nfs_exports_record="\"${vagrant_dir}\" 172.17.0.0/255.255.0.0(rw,no_subtree_check,all_squash,anonuid=2000,anongid=2000)"
-    nfs_exports_record="${vagrant_dir} 172.17.0.1(rw,all_squash,anonuid=0,anongid=0)"
+    nfs_exports_record="\"${vagrant_dir}\" 172.17.0.0/255.255.0.0(rw,no_root_squash,anonuid=0,anongid=0)"
 else
     error "Host OS is not supported"
-    exit 1
+    exit 1172.17.0.1:/home/travis/build/paliarush/magento2-kubernetes-devbox/tests/tmp/test/magento2-vagrant
 fi
 
 echo "${nfs_exports_record}"

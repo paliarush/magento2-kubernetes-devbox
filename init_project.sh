ed0f84f9fcba597a49d59698da70b48a03d9d46c
Alex Paliarush <paliarus@adobe.com>
2019-04-16 11:17:19 -0500

    NFS 1

--- init_project.sh
+++ init_project.sh
@@ -216,6 +216,11 @@ if [[ ! -d ${magento_ce_dir} ]]; then
     fi
 fi
 
+#status "Making magento files readable and writable by anybody"
+#sudo chmod -R a+wx "${vagrant_dir}"
+#sudo chown -R travis:travis "${vagrant_dir}"
+#sudo stat "${vagrant_dir}"
+
 status "Initializing dev box"
 cd "${vagrant_dir}"
 

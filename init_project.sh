31a0918db5e18776a7e7daaf2fdd9f7032019a52
Alex Paliarush <paliarus@adobe.com>
2019-04-17 00:08:06 -0500

    init-container

--- init_project.sh
+++ init_project.sh
@@ -223,6 +223,17 @@ sudo chmod -R a+rwx "${vagrant_dir}"
 sudo chown -R nobody:nogroup "${vagrant_dir}"
 sudo stat "${vagrant_dir}"
 
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -F
+
 status "Initializing dev box"
 cd "${vagrant_dir}"
 

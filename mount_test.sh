7c98bfce4cbe549ce442d4d37a3400271009b3e2
Alex Paliarush <paliarus@adobe.com>
2019-04-17 00:05:44 -0500

    init-container

--- mount_test.sh
+++ mount_test.sh
@@ -4,6 +4,6 @@ mkdir -pv /var/test
 chmod a+w /var/test
 showmount -e 172.17.0.1
 echo "Before Mount"
-mount 172.17.0.1:/home/travis/build/paliarush/magento2-kubernetes-devbox/tests/tmp/test/magento2-vagrant /var/test
+mount -t nfs 172.17.0.1:/home/travis/build/paliarush/magento2-kubernetes-devbox/tests/tmp/test/magento2-vagrant /var/test
 mount -l
 echo "After Mount"

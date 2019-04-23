f9cee011ac78a1771bef02cb9dbc05b53808da94
Alex Paliarush <paliarus@adobe.com>
2019-04-23 17:35:45 -0500

    elastic

--- scripts/host/configure_ubuntu_travis.sh
+++ scripts/host/configure_ubuntu_travis.sh
@@ -7,7 +7,7 @@ sudo ./scripts/host/configure_nfs_exports.sh
 cp ./tests/include/configuration.sh.dist ./tests/include/configuration.sh
 sed -i "s|git@github.com:|https://github.com/|g" ./etc/config.yaml.dist
 sed -i "s|minikube start --cpus=2 --memory=4096|sudo minikube start --cpus=2 --memory=4096  --vm-driver=none --bootstrapper=kubeadm --kubernetes-version=v1.13.0|g" ./init_project.sh
-sed -i "s|git clone|git clone --depth 1 |g" ./init_project.sh
+#sed -i "s|git clone|git clone --depth 1 |g" ./init_project.sh
 sed -i "s|&& eval \$(minikube docker-env) ||g" ./scripts/host/k_rebuild_environment.sh
 sed -i "s/use_nfs:\ 1/use_nfs:\ 0/g" ./etc/config.yaml.dist
 sed -i "s/enabled:\ true \# NFS Status/enabled:\ false \# NFS Status/g" ./etc/helm/values.yaml

60f2f6fb19bf21f75e74a5f9281e53697043903a
Alex Paliarush <paliarus@adobe.com>
2019-05-24 14:27:12 -0500

    Storefront-service

--- scripts/host/k_rebuild_environment.sh
+++ scripts/host/k_rebuild_environment.sh
@@ -13,8 +13,8 @@ cd "${devbox_dir}/scripts" && eval $(minikube docker-env) && docker build -t mag
 
 
 # TODO: Delete does not work when no releases created yet
-cd "${devbox_dir}/etc/helm"
-bash "${devbox_dir}/scripts/host/helm_delete_wait.sh" magento2
+#cd "${devbox_dir}/etc/helm"
+#bash "${devbox_dir}/scripts/host/helm_delete_wait.sh" magento2
 
 # TODO: Need to make sure all resources have been successfully deleted before the attempt of recreating them
 #sleep 20
@@ -32,8 +32,18 @@ use_nfs="$(bash "${devbox_dir}/scripts/get_config_value.sh" "guest_use_nfs")"
 
 status "Deploying cluster, it may take several minutes"
 
-cd "${devbox_dir}/etc/helm" && helm install \
-    --name magento2 \
+#cd "${devbox_dir}/etc/helm" && helm install \
+#    --name magento2 \
+#    --values values.yaml \
+#    --wait \
+#    --set global.persistence.nfs.serverIp="${nfs_server_ip}" \
+#    --set global.monolith.volumeHostPath="${devbox_dir}" \
+#    --set global.persistence.nfs.enabled="$(if [[ ${use_nfs} == "1" ]]; then echo "true"; else echo "false"; fi)" \
+#    --set global.caching.varnish.enabled="$(if [[ ${use_varnish} == "1" ]]; then echo "true"; else echo "false"; fi)" \
+#    --set global.checkout.enabled="${enable_checkout}" \
+#    --set global.checkout.volumeHostPath="${devbox_dir}" .
+
+cd "${devbox_dir}/etc/helm" && helm upgrade \
     --values values.yaml \
     --wait \
     --set global.persistence.nfs.serverIp="${nfs_server_ip}" \
@@ -41,7 +51,7 @@ cd "${devbox_dir}/etc/helm" && helm install \
     --set global.persistence.nfs.enabled="$(if [[ ${use_nfs} == "1" ]]; then echo "true"; else echo "false"; fi)" \
     --set global.caching.varnish.enabled="$(if [[ ${use_varnish} == "1" ]]; then echo "true"; else echo "false"; fi)" \
     --set global.checkout.enabled="${enable_checkout}" \
-    --set global.checkout.volumeHostPath="${devbox_dir}" .
+    --set global.checkout.volumeHostPath="${devbox_dir}" magento2 .
 
 # TODO: Waiting for containers to initialize before proceeding
 #waitForKubernetesPodToRun 'tiller-deploy'
